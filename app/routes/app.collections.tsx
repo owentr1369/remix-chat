fba10fe54dc33baf50d1f5b05443324ce0b535b8
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-02-28 17:57:31 +0700

    add loader function to fetch collections from Shopify API

--- app/routes/app.collections.tsx
+++ app/routes/app.collections.tsx
@@ -1,4 +1,40 @@
+import type { LoaderFunction } from "@remix-run/node";
 import { Card, Layout, Page } from "@shopify/polaris";
+import { authenticate, apiVersion } from "app/shopify.server";
+
+export const query = `{
+  collections(first)
+}`;
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+  const { shop, accessToken } = session;
+
+  try {
+    const response = await fetch(
+      `https://${shop}/admin/api/${apiVersion}/graphql.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": accessToken,
+        },
+        body: query,
+      },
+    );
+    if (response.ok) {
+      const data = await response.json();
+      const {
+        data: {
+          collections: { edges },
+        },
+      } = data;
+      return edges;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const Collections = () => {
   return (
