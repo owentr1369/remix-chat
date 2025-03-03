96726ee63c6617aa4122329243af95e3db784f7a
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-03-03 10:46:13 +0700

    add Inventory route to fetch and display inventory levels from Shopify API

---
+++ app/routes/app.inventory.tsx
@@ -0,0 +1,32 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { authenticate, apiVersion } from "app/shopify.server";
+import { Card, Layout, List, Page } from "@shopify/polaris";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { session, admin } = await authenticate.admin(request);
+
+  try {
+    const response = await fetch(
+      `https://${session.shop}/admin/api/${apiVersion}/inventory_levels.json?location_id=69259657334`,
+      {
+        headers: {
+          "X-Shopify-Access-Token": session.accessToken || "",
+        },
+      },
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
+
+const AppInventory = () => {
+  const inventoryLevels = useLoaderData() as any[];
+  console.log(inventoryLevels, "inventoryLevels");
+  return <div>AppInventory</div>;
+};
+
+export default AppInventory;
